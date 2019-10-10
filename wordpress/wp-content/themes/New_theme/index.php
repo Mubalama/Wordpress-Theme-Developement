e9014c9a9e560c08571e90e995b5a16227de3abc
Mubalama Richard <mubalama@live.com>
2019-10-11 04:03:28 +1100

    Added Files

---
+++ wordpress/wp-content/themes/New_theme/index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  The template for displaying the header
+*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-with, initial-scal=1">
+		<?php wp_head(); ?>
+	</head>
+
+
+	<body <?php body_class(); ?> >
+
+	<h1>This the Index file</h1>
+
+
+		<?php wp_footer(); ?>
+	</body>
+
+
+</html>
