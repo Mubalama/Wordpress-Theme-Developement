4a18ce3dba9d91ae9db83b66e82361025c32cb71
Mubalama Richard <mubalama@live.com>
2019-10-11 03:14:04 +1100

    Files added

---
+++ wordpress/wp-content/themes/New_theme/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+// My theme functions
+
+
+//Theme Title tag
+
+add_theme_support('title-tag');
---
+++ wordpress/wp-content/themes/New_theme/index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  The template for displaying the header
+*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-with, initial-scal=1">
+		<?php wp_head(); ?>
+	</head>
+
+
+	<body body_class(); ?>>
+
+
+		<?php wp_footer(); ?>
+	</body>
+
+
+</html>
